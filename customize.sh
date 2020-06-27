0e62c85bff5e8c83740d823f9508754ff9ab283f
Aromalct <34676548+Aromalct@users.noreply.github.com>
2020-06-27 20:04:33 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -8,11 +8,8 @@
 #1. 修改默认IP配置
 sed -i 's/192.168.1.1/192.168.2.2/g' openwrt/package/base-files/files/bin/config_generate
 
-#2. 修改system配置
+#2. 修改默认登录密码
+sed -i 's/root::0:0:99999:7:::/root:$1$6CWbO1jh$mGZ1whdJxPQJVpO4OFMnc.:18276:0:99999:7:::/g' openwrt/package/base-files/files/etc/shadow
+
+#3. 修改时区配置
 sed -i 's/UTC/CST-8/g' openwrt/package/base-files/files/bin/config_generate
-sed -i 's/OpenWrt/Aroma-OpenWrt/g' openwrt/package/base-files/files/bin/config_generate
-
-
-
-
-
